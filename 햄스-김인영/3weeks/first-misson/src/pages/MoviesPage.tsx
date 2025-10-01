7c893c918cc767cd5b6b5c36a899f7ca96fdd589
inyouxg <dolley03@naver.com>
2025-10-02 00:48:24 +0900

    feat: MovieCard 컴포넌트화 + style 추가
    first-mission 마무리

--- 햄스-김인영/3weeks/first-misson/src/pages/MoviesPage.tsx
+++ 햄스-김인영/3weeks/first-misson/src/pages/MoviesPage.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react"
 import type { Movie, MovieResponse } from "../types/movies";
 import axios from "axios";
+import { MovieCard } from "../components/MovieCard";
 
-export const MoviesPage = () => {
+export const MoviesPage = (): React.ReactElement => {
   const [movies, setMovies] = useState<Movie[]>([]);
 
   useEffect(() => {
@@ -22,6 +23,12 @@ export const MoviesPage = () => {
 
   console.log(movies);
   return (
-    <div></div>
+    <div>
+      <div className="p-10 grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
+        {movies.map((movie) => {
+            return <MovieCard key={movie.id} movie={movie} />
+          })}
+      </div>
+    </div>
   )
 }
