8f7f783af1d526393ff9a3969badc8c49477dcc9
inyouxg <dolley03@naver.com>
2025-10-16 00:41:52 +0900

    feat: navbar, router 추가

--- 햄스-김인영/4weeks/second-mission/src/App.tsx
+++ 햄스-김인영/4weeks/second-mission/src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css'
+import {RouterProvider} from 'react-router-dom'
+import router from './routers/router.tsx'
 
 function App() {
   return (
-    <>
-    </>
+    <RouterProvider router={router} />
   )
 }
 
