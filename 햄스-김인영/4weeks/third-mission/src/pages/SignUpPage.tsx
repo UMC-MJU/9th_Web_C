f22eca9c383c25321a64f2ea0e04c173a72b8dbc
inyouxg <dolley03@naver.com>
2025-10-29 20:07:15 +0900

    feat: 다단계 폼 전환 구현
    email, password, name 컴포넌트 분리

--- 햄스-김인영/4weeks/third-mission/src/pages/SignUpPage.tsx
+++ 햄스-김인영/4weeks/third-mission/src/pages/SignUpPage.tsx
@@ -1,6 +1,10 @@
 import {z} from "zod";
 import { useForm, type SubmitHandler } from "react-hook-form";
 import {zodResolver} from "@hookform/resolvers/zod";
+import { InputEmail } from "../components/InputEmail";
+import { InputPassword } from "../components/InputPassword";
+import { InputName } from "../components/InputName";
+import { useState } from "react";
 
 /*유효성 검사 스키마*/
 const schema = z.object({
@@ -24,13 +28,15 @@ const schema = z.object({
 });
 
 /*스키마로부터 자동으로 typescript 타입 유추*/
-type FormFields = z.infer<typeof  schema>;
+export type FormFields = z.infer<typeof  schema>;
 
 const SignUpPage = () => {
+  const [state, setState] = useState<string>("email");
+
   /*useForm의 method 구조분해할당*/
   const {register, 
     handleSubmit, 
-    formState: {errors, isSubmitting},
+    formState: {errors, isSubmitting, isValid},
   } = useForm<FormFields>({
     defaultValues: {
       email: "",
@@ -39,13 +45,14 @@ const SignUpPage = () => {
       passwordCheck: "",
     },
     resolver: zodResolver(schema),
-    mode: "onBlur",
+    mode: "onChange",
   })
 
   const onSubmit:SubmitHandler<FormFields> = (data) => {
     const{passwordCheck, ...rest} = data;
     console.log(rest);
   }
+
   return (
     <div className="flex justify-center items-center grow bg-gray-50">
       <div className="flex flex-col w-70 gap-3">
@@ -57,62 +64,29 @@ const SignUpPage = () => {
           <span className="mx-3 text-gray-400 text-sm font-medium">OR</span>
           <div className="grow border-t border-gray-400"></div>
         </div>
-        <input
-          {...register('email')}
-          name="email"
-          className={`border border-gray-400 rounded-lg focus:outline-none p-2 w-full text-sm text-gray-500 bg-white
-          ${errors?.email ? "border-red-400 bg-red-200" : "border-gray-400"}`}
-          type={"email"}
-          placeholder="email"
+        {state === "email" && <InputEmail
+          register={register}
+          errors={errors}
+          setState={setState}
         />
-        {errors?.email && (
-          <div className="text-red-400 text-[13px]">{errors.email.message}</div>
-        )}
-
-        <input
-          {...register("password")}
-          name="password"
-          className={`border border-gray-400 rounded-lg focus:outline-none p-2 w-full text-sm text-gray-500 bg-white
-          ${errors?.password ? "border-red-400 bg-red-200" : "border-gray-400"}`}
-          type={"password"}
-          placeholder="password"
-        />
-        {errors?.password && (
-          <div className="text-red-400 text-[13px]">{errors.password.message}</div>
-        )}
-        <input
-          {...register("passwordCheck")}
-          name="passwordCheck"
-          className={`border border-gray-400 rounded-lg focus:outline-none p-2 w-full text-sm text-gray-500 bg-white
-          ${errors?.password ? "border-red-400 bg-red-200" : "border-gray-400"}`}
-          type={"password"}
-          placeholder="password-check"
-        />
-        {errors?.passwordCheck && (
-          <div className="text-red-400 text-[13px]">{errors.passwordCheck.message}</div>
-        )}
-
-        <input
-          {...register("name")}
-          name="name"
-          className={`border border-gray-400 rounded-lg focus:outline-none p-2 w-full text-sm text-gray-500 bg-white
-          ${errors?.password ? "border-red-400 bg-red-200" : "border-gray-400"}`}
-          type={"name"}
-          placeholder="name"
-        />
-        {errors?.name && (
-          <div className="text-red-400 text-[13px]">{errors.name.message}</div>
-        )}
-
-        <button
-          className="bg-blue-300 rounded-lg focus:outline-none p-3 w-full text-sm text-white font-semibold
-        disabled:bg-gray-400 disabled:cursor-not-allowed"
-          type="submit"
-          disabled={isSubmitting}
-          onClick={handleSubmit(onSubmit)}
-        >
-          회원가입 완료
-        </button>
+        }
+        {state === "password" &&
+          <InputPassword
+            register={register}
+            errors={errors}
+            setState={setState}
+          />
+        }
+        {state === "name" &&
+          <InputName
+            register={register}
+            errors={errors}
+            setState={setState}
+            isValid={isValid}
+            handleSubmit={handleSubmit}
+            onSubmit={onSubmit}
+          />
+        }
       </div>
     </div>
   )
