0104451e18bd149caa1f52ea8506396743d48ef6
Minseok Kang <columenk50@gmail.com>
2025-11-04 01:47:01 +0900

    5주차 과제 1,2,3

---
+++ 블루-강민석/week5/chapter5-3/src/components/Navbar.tsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="w-full flex justify-between items-center px-6 py-3 bg-black text-white">
+      {/* 왼쪽 타이틀 */}
+      <Link
+        to="/"
+        className="text-pink-400 text-xl font-bold"
+      >
+        MyApp
+      </Link>
+
+      {/* 오른쪽 버튼들 */}
+      <div className="flex items-center gap-3">
+        <Link
+          to="/login"
+          className="bg-black text-white border border-white px-4 py-2 rounded hover:bg-white hover:text-black transition"
+        >
+          로그인
+        </Link>
+        <Link
+          to="/signup"
+          className="bg-pink-500 text-white px-4 py-2 rounded hover:bg-pink-600 transition"
+        >
+          회원가입
+        </Link>
+      </div>
+    </nav>
+  );
+}
