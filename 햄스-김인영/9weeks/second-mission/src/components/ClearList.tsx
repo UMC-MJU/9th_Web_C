913e60265573b80b04db03e1715d1f3bd4173eef
inyouxg <dolley03@naver.com>
2025-12-20 19:09:19 +0900

    feat : modal component 분리 및 적용

--- 햄스-김인영/9weeks/second-mission/src/components/ClearList.tsx
+++ 햄스-김인영/9weeks/second-mission/src/components/ClearList.tsx
@@ -1,21 +1,40 @@
 import { useDispatch, useSelector } from "../hooks/useAppDispatch"
 import { clearCart } from "../slices/cartSlice";
+import { closeModal, openModal } from "../slices/modalSlice";
+import { Modal } from "./Modal";
 
 export const ClearList = () => {
   const {total} = useSelector((state) => state.cart);
+  const isOpen = useSelector((state) => state.modal.isOpen);
   const dispatch = useDispatch();
 
   const handleClearList = () => {
     dispatch(clearCart());
+    dispatch(closeModal());
   }
+
+  const handleOpen = () => {
+    dispatch(openModal());
+  }
+  const handleClose = () => {
+    dispatch(closeModal());
+  }
+
   return (
-    <div className="flex justify-center items-center">
-      <div className="flex justify-between items-center w-210">
-        <button 
-        onClick={handleClearList}
-        className="bg-gray-200 hover:bg-gray-300 font-semibold px-5 py-3 rounded-sm m-4">장바구니 비우기</button>
-        <div className="m-4">{total}원</div>
+    <>
+      <div className="flex justify-center items-center">
+        <div className="flex justify-between items-center w-210">
+          <button
+            onClick={handleOpen}
+            className="bg-gray-200 hover:bg-gray-300 font-semibold px-5 py-3 rounded-sm m-4">장바구니 비우기</button>
+          <div className="m-4">{total}원</div>
+        </div>
       </div>
-    </div>
+      <Modal
+        isOpen={isOpen}
+        onClose={handleClose}
+        onClearList={handleClearList}
+      />
+    </>
   )
 }
